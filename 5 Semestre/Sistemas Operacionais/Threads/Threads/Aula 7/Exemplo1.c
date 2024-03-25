f4b86381344edaf94137b50cf45e9a431965f217
AugustoPB <106416779+AugustoPBaldino@users.noreply.github.com>
2024-03-25 17:11:20 -0300

    Atualização Sisop

---
+++ 5 Semestre/Sistemas Operacionais/Threads/Threads/Aula 7/Exemplo1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// Criação de uma thread e compartilhamento de dados
+int global;
+void *thr_func(void *arg);
+
+int main(void)
+{
+	pthread_t tid;
+
+	global = 20;
+
+	printf("Thread principal: %d\n", global);
+	pthread_create(&tid, NULL, thr_func, NULL);
+	pthread_join(tid, NULL);
+	printf("Thread principal: %d\n", global);
+
+	return 0;
+}
+
+void *thr_func(void *arg)
+{
+	global = 40;
+	printf("Nova thread: %d\n", global);
+
+	return 0;
+}
